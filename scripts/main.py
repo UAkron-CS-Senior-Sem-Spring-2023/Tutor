5674f1beda6ac367f85c1bac4bc5cea60326238b
MrRamPoudel <rp164@uakron.edu>
2023-02-22 17:02:27 -0500

    Add fake student info

--- scripts/main.py
+++ scripts/main.py
@@ -2,24 +2,44 @@
 from faker import Faker
 import csv
 faker = Faker()
-
+Faker.seed(1000);
 #number of fake students
 totalInfo = 4000;
 
+def eraseTitle(fullname):
+    #Erase the title
+    if '.' in fullname:
+        fullname = fullname.split()
+        fullname.pop(0)
+    else:
+        fullname = fullname.split()
+    return fullname
+
+def getFirstandLast(fullname):
+     #extract first and last name
+    first = fullname[0]
+    #incase it has middle name
+    if(len(fullname) == 3):
+        last = fullname[2]
+    else:
+        last = fullname[1]
+    return first,last
+
 with open('students.csv','w', newline='') as file:
     writer = csv.writer(file,delimiter = ',')
-    writer.writerow(['First name', 'Last Name', 'StudentID', 'email'])
+    writer.writerow(['First name', 'Last Name', 'studentID','Password', 'email'])
+    x = {"initial":[],"count":[]}
     for i in range(1,totalInfo+1):
-        fullname = faker.name().split()
-        first = fullname[0]
-        #incase it has middle name
-        if(len(fullname) == 3):
-            last = fullname[2]
+        num = faker.unique.numerify('######')
+        fullname = eraseTitle(faker.name())
+        first,last = getFirstandLast(fullname)
+        initial = first[0] + last[0]
+        if(initial in x):
+            x[initial] += 1
         else:
-            last = fullname[1]
-        if(fullname.count(' ') == 2):
-            first,last = fullname.split()
-        
-        email = first[0] + last[0] + str(i) + "@example.com"
-        row = [first, last, "{:04d}".format(i), email.lower()]
+            x[initial] = 1
+            
+        getNum = x[initial]
+        email = first[0] + last[0] + str(x[initial]) + "@example.com"
+        row = [first, last, num,last + str(num),  email.lower()]
         writer.writerow(row)
