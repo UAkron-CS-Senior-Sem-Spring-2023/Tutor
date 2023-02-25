08d1b5adcc51bbba9726f81c80d2b80e04859e60
MrRamPoudel <rp164@uakron.edu>
2023-02-24 23:11:03 -0500

    Add input field to index.html

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -7,6 +7,10 @@ const socket = io.connect("http://localhost:3001");
 function App() {
   return (
     <div className="App">
+    <h1> Join a room</h1>
+    <input type="text" placeholder="Your name"></input>
+    <input type="text" placeholder="Room id" ></input>
+    <button> Join</button>
     </div>
   );
 }
