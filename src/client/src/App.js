65a1e7f628123ceb688cce942269484129dddb1d
MrRamPoudel <rp164@uakron.edu>
2023-02-24 22:56:16 -0500

    Fix socket.io includes

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import io from 'socket.io-client';
+
+//Connect to backend
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   return (
--- src/server/index.js
+++ src/server/index.js
@@ -2,22 +2,24 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const {Server } = require("socket.io");
 
 app.use(cors());
 
 
 const server = http.createServer(app);
 
+//localhost:3000 is the client side
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost/3000",
+        origin: "http://localhost:3000",
         methods:["GET", "POST"],
     },
 });
 
 //someone has connected
 io.on("connection", (socket)=>{
-    console.log(socket.id);
+    console.log(`User Connected: ${socket.id}`);
     socket.on("disconnect", () =>{
         console.log("User disconnected", socket.id);
     });
