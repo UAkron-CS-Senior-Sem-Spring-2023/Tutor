5b1c52bb5efde2d65b67b3eab9b77afc7427b83f
MrRamPoudel <rp164@uakron.edu>
2023-02-24 22:47:49 -0500

    Add React files

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,22 @@ app.use(cors());
 
 const server = http.createServer(app);
 
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost/3000",
+        methods:["GET", "POST"],
+    },
+});
+
+//someone has connected
+io.on("connection", (socket)=>{
+    console.log(socket.id);
+    socket.on("disconnect", () =>{
+        console.log("User disconnected", socket.id);
+    });
+});
+
+
 const port = 3001;
 server.listen(port, () => {
     console.log("server is running");
