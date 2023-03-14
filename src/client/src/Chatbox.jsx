fb47e421986c071cd15ae9c9a39f313bb3054781
MrRamPoudel <rp164@uakron.edu>
2023-03-14 16:47:11 -0400

    Show message as h2 tag

--- src/client/src/Chatbox.jsx
+++ src/client/src/Chatbox.jsx
@@ -3,35 +3,33 @@ import React , {useState, useEffect} from "react";
 export default function Chat({socket, name, roomID}) {
     //current message
     const [message, setMessage] = useState("");
+    //save all messages
+    const [allMessages, setAllMessage] = useState([]);
     
    const sendMessage = async () =>{
-        //We want to send the name, roomID, message and timestamp
+        //We want to send the name, roomID, message
         if(message !== ""){
-            const now = new Date();
-            const hour = now.getHours();
-            const minute = now.getMinutes();
-            const second = now.getSeconds();
             const data ={
                 roomID: roomID,
                 name: name,
                 message:message,
-                timestamp: `${hour}:${minute}:${second}`,
             };
             await socket.emit("sendMessage", data);
         }
     };
     useEffect(() => {
         socket.on("receiveMsg", (data) =>{
-            console.log(data);
+            setAllMessage(list => [...list, data]);
         });
     }, [socket]);
 
     //TODO, EXPORT THIS
     return (<div><div className="top"> <h3> Live Chat </h3></div>
              <div className="message"> 
-              <input type="text" placeholder="Hello!" onChange={(event)=>setMessage(event.target.value)}></input>
-              <button onClick={sendMessage}>Send</button>
+              {allMessages.map(eachMessage => (<h2>{eachMessage.message}</h2>))}
               </div>
-            <div className="bottom"> </div>
+            <div className="bottom">
+            <input type="text" placeholder="Hello!" onChange={(event)=>setMessage(event.target.value)} onKeyDown ={(event)=>event.key === 'Enter' && sendMessage()}></input>
+              <button onClick={sendMessage}>Send</button> </div>
             </div>);
 }
